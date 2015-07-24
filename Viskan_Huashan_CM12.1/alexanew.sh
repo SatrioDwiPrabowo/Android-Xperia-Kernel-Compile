21895756c257d3a95e873dabb5eacbecae3be650
SatrioDwiPrabowo <sdprabowo77@gmail.com>
2015-07-24 11:51:51 +0700

    "Alexa Configuration for Xperia SP"

---
+++ Viskan_Huashan_CM12.1/alexanew.sh
@@ -0,0 +1,1 @@
+python mkelf.py -o AlexaKernel.img zImage@0x80208000 AlexaRamdisk.img@0x81900000,ramdisk RPM_JB-4.3.bin@0x00020000,rpm bootcmd_JB-4.3@0x00000000,cmdline
---
+++ Viskan_Huashan_CM12.1/c.sh
@@ -0,0 +1,1 @@
+python mkelf.py -o alexacm12aicp.img zImage@0x80208000 initrd.img@0x81900000,ramdisk RPM_JB-4.3.bin@0x00020000,rpm bootcmd_JB-4.3@0x00000000,cmdline
